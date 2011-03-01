4dcd096eab85621be0da8aec5d9f3fb6268f3784
Carin Meier <cmeier@Carin-Meiers-MacBook-Air.local>
2011-02-28 21:17:15 -0500

    inital checkin

---
+++ README.markdown
@@ -0,0 +1,16 @@
+# bowling-game
+
+This is the Bowling Game Kata done in Clojure with STM. The tests use Midje.  The original kata is here in Java.  http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
+
+## Usage
+
+install Leiningen https://github.com/technomancy/leiningen
+lein deps
+lein test
+
+
+## License
+
+Copyright (C) 2011 Carin Meier
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ test/bowling_game/test/game.clj
@@ -0,0 +1,61 @@
+(ns bowling-game.test.game
+  (:use [bowling-game.game] :reload)
+  (:use [clojure.test])
+  (:use [midje.sweet]))
+
+
+(defn roll-many [n, pins]
+  (dorun (map roll (repeat n pins))))
+
+(defn roll-spare []
+  (do
+    (roll 5)
+    (roll 5)))
+
+(defn roll-strike []
+  (do
+    (roll 10)))
+
+(deftest testGutterGame
+  (do
+    (new-game)
+    (roll-many 20 0)
+    (fact (score) => 0)))
+
+(deftest testAllOnes
+  (do
+    (new-game)
+    (roll-many 20 1)
+    (fact (score) => 20)))
+
+
+(deftest testOneSpare
+  (do
+    (new-game)
+    (roll-spare)
+    (roll 3)
+    (roll-many 16 0)
+    (fact (score) => 16)))
+
+(deftest testOneStrike
+  (do
+    (new-game)
+    (roll-strike)
+    (roll 3)
+    (roll 4)
+    (roll-many 16 0)
+    (fact (score) => 24 )))
+
+(deftest testPerfectGame
+  (do
+    (new-game)
+    (roll-many 12 10)
+    (fact (score) => 300)
+    ))
+
+(deftest testAllFivesGame
+  (do
+    (new-game)
+    (roll-many 21 5)
+    (fact (score) => 150)
+    ))
