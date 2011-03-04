0b73835d01f994fc95155f19543b8215624fe34a
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-03 21:01:57 -0500

    renames for develoepr redability

--- src/bowling_game/game.clj
+++ src/bowling_game/game.clj
@@ -20,15 +20,17 @@
 (defn new-game []
   (reset! current-game (construct-game)))
 
-(defvar- rolls-in-frame (comp count :pins-hit))	
+(defvar- rolls-already-completed (comp count :pins-hit))	
 
-(defvar- first-roll (comp first :pins-hit))	  
+(defvar- first-roll (comp first :pins-hit))	 
 
-(defvar- sum-frame (comp sum :pins-hit))
+(defvar- second-roll (comp second :pins-hit)) 
 
-(defvar- all-pins-down? (comp (partial = 10) sum-frame))
+(defvar- pins-knocked-down (comp sum :pins-hit))
+
+(defvar- all-pins-down? (comp (partial = 10) pins-knocked-down))
   
-(defvar- all-rolls-done? (comp (partial = 2) rolls-in-frame))
+(defvar- all-rolls-done? (comp (partial = 2) rolls-already-completed))
 	
 (defn- start-new-frame? [frame num-frames]
   (and (or (all-pins-down? frame) (all-rolls-done? frame))
@@ -58,52 +60,51 @@
        (add-pins-to-frames (:frames @current-game) pins))))
 	
 (defn- score-for-frame-is-10-and-on-roll-x? [rolls-already-rolled frame]
-    (and (= rolls-already-rolled (rolls-in-frame frame)) 
-	     (= 10 (sum-frame frame))))
+    (and (= rolls-already-rolled (rolls-already-completed frame)) 
+	     (= 10 (pins-knocked-down frame))))
 	
 (defvar- spare? (partial score-for-frame-is-10-and-on-roll-x? 2))
 
 (defvar- strike? (partial score-for-frame-is-10-and-on-roll-x? 1))
 
 (defn- next-roll-in-frames [[first-frame & _]]
-  (first (:pins-hit first-frame)))
+  (first-roll first-frame))
 
 (defn- next-two-rolls-in-frames [[first-frame next-frame & _ :as frames]]
   (let [last-frame? (nil? next-frame)]
     (cond last-frame?
-         (+ (next-roll-in-frames frames) (second (:pins-hit first-frame)))
+         (+ (next-roll-in-frames frames) (second-roll first-frame))
          (strike? first-frame)
          (+ 10 (next-roll-in-frames frames))
 		 :else
-         (sum-frame first-frame))))
-
-(defvar- next-roll-in-final-frame (comp second :pins-hit))
-
-(defn- next-two-rolls-in-final-frame [frame]
-  (let [roll1 (second (:pins-hit frame))
-        roll2 (last (:pins-hit frame))]
-    (+ roll1 roll2)))
+         (pins-knocked-down first-frame))))
 
 (defn- score-current-frame [[first-frame & rest-frames]]
-  (cond (spare? first-frame)
-        (+ (sum-frame first-frame) (next-roll-in-frames rest-frames))
-      (strike? first-frame)
-        (+ (sum-frame first-frame) (next-two-rolls-in-frames rest-frames))
-      :else  
-	  (sum-frame first-frame)))
+  (let [score (pins-knocked-down first-frame)]
+     (cond (spare? first-frame)
+        (+ score (next-roll-in-frames rest-frames))
+        (strike? first-frame)
+        (+ score (next-two-rolls-in-frames rest-frames))
+        :else  
+	    score)))
+	
+(defvar- next-roll-in-last-frame (comp first :pins-hit))
+
+(defn- next-two-rolls-in-last-frame [frame]
+  (+ (first-roll frame) (second-roll frame)))	
 	  
 (defn- score-last-frame [frame]
     (cond (spare? frame)
-          (+ (first-roll frame) (next-roll-in-final-frame frame))
+          (+ (first-roll frame) (next-roll-in-last-frame frame))
           (strike? frame)
-          (+ (first-roll frame) (next-two-rolls-in-final-frame frame))
+          (+ (first-roll frame) (next-two-rolls-in-last-frame frame))
 		  :else
-          (sum-frame frame)))
+          (pins-knocked-down frame)))
 
-(defn- sum-frames [score [first-frame & rest-frames :as frames-left]]
+(defn- score-frames [score [first-frame & rest-frames :as frames-left]]
     (if (= 1 (count frames-left))
       (+ score (score-last-frame first-frame))
-      (+ score (sum-frames (score-current-frame frames-left) rest-frames))))
+      (+ score (score-frames (score-current-frame frames-left) rest-frames))))
 
 (defn score []
-  (sum-frames 0 (:frames @current-game)))
+  (score-frames 0 (:frames @current-game)))
